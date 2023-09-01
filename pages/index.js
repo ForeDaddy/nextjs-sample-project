481aa7cea06ef88881d25ab09e7f96b46ee5eeef
Donn Neufeld <donn.neufeld@gmail.com>
2023-09-01 10:40:34 -0700

    first page name and bio

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,56 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+      <section className={utilStyles.headingMd}>
+        <p>
+          As a passionate and skilled full stack software engineer, I am excited
+          to bring my diverse background to solving today's toughest challenges.
+          With experience in real estate, technology sales, and management, I
+          have developed a unique skill set that allows me to excel in a variety
+          of contexts.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+        <p>
+          My technical skills include expertise in several programming
+          languages, databases, and frameworks, including JavaScript, React,
+          Node.js, MySQL, and MongoDB. I have worked on a range of projects,
+          from developing front-end interfaces to designing and implementing
+          back-end systems. One project I am particularly proud of is
+          StackageApp; a Full Stack React-native project where I was part of an
+          8 member team of software engineers that designed and built a
+          user-moderated social media application for sharing science/technology
+          related content.
+        </p>
+        <p>
+          As a problem-solver, I thrive in challenging situations and enjoy
+          finding creative solutions to complex problems. In my previous roles,
+          I was able to use my problem-solving abilities to overcome obstacles
+          and deliver results. For example, when I was working in Global
+          Business Development, I was tasked with increasing revenue in a
+          challenging market. By analyzing data, developing new strategies, and
+          collaborating with my team, we were able to deploy new strategies for
+          lead engagement and platform sales. My experience as a real estate
+          agent and manager has also taught me valuable skills that are relevant
+          to software engineering. As a successful agent, I developed strong
+          communication and negotiation skills, which have proved invaluable
+          when working with clients and stakeholders. As a manager, I was
+          responsible for leading and motivating a team that I hired, which has
+          helped me to develop excellent leadership and mentoring skills.
+        </p>
+        <p>
+          I am excited about the opportunity to bring my skills and experience
+          to a full stack software engineering role and make a meaningful
+          contribution to a team. Please feel free to contact me if you would
+          like to discuss any potential mutual fits with your current
+          project/hiring needs.
+        </p>
+      </section>
+    </Layout>
+  );
 }
