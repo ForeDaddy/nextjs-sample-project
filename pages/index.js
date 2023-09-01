0261896dee155ba0508de9cd319a2326cdad077d
Donn Neufeld <donn.neufeld@gmail.com>
2023-09-01 12:08:46 -0700

    file read, static rendering

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,17 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -50,6 +60,20 @@ export default function Home() {
           like to discuss any potential mutual fits with your current
           project/hiring needs.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
